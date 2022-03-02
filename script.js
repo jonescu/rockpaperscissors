87d9a30627778750006b7cc4eb3d591f514028e9
Nick Jonescu <nicholasjonescu@gmail.com>
2022-03-02 18:02:39 -0500

    Added a few functions

---
+++ script.js
@@ -0,0 +1,26 @@
+//Randomly selects from an array of options
+function computerPlay(){
+    const choices = ["Rock","Paper","Scissors"];
+    return choices[Math.floor(Math.random()* 3)];
+}
+
+// Plays one round, playerSelection is case insensitive. Returns the winner or if the game ties.
+function playRound(playerSelection, computerSelection) {
+    const firstChar = playerSelection[0].toUpperCase();
+    const player = playerSelection.slice(1).toLowerCase();
+    const final =  firstChar.concat(player)
+    console.log(final, computerSelection)
+    if(final === computerSelection) {
+        return "Tie, try again"
+    } else if(final === "Rock" && computerSelection !== "Paper") {
+        return "Player wins"
+    } else if(final === "Paper" && computerSelection !== "Scissors"){
+        return "Player wins"
+    } else if(final === "Scissors" && computerSelection !== "Rock"){
+        return "Player wins"
+    } else {
+        return "Computer wins"
+    }
+
+  }
+  

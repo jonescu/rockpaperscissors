165383c498f1c80dbcce305238647e612e4ff6b4
Nick Jonescu <nicholasjonescu@gmail.com>
2022-03-02 19:09:27 -0500

    added more functions

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function computerPlay(){
 
 // Plays one round, playerSelection is case insensitive. Returns the winner or if the game ties.
 function playRound(playerSelection, computerSelection) {
+   playerSelection = prompt('Make a selection: Rock, Paper, or Scissors')
+   computerSelection = computerPlay();
     const firstChar = playerSelection[0].toUpperCase();
     const player = playerSelection.slice(1).toLowerCase();
     const final =  firstChar.concat(player)
-    console.log(final, computerSelection)
+    console.log(final, computerSelection);
     if(final === computerSelection) {
         return "Tie, try again"
     } else if(final === "Rock" && computerSelection !== "Paper") {
@@ -21,6 +23,13 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return "Computer wins"
     }
+  }
 
+//Play five rounds
+  function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+     }
   }
   
+  game();

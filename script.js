7c0a6e8185ca6b7b1498dc0ed2d97603992940c3
Nick Jonescu <nicholasjonescu@gmail.com>
2022-05-02 18:58:05 -0400

    finished basic ui

--- script.js
+++ script.js
@@ -7,41 +7,79 @@ function computerPlay(){
 
 // Plays one round, playerSelection is case insensitive. Returns the winner or if the game ties.
 
+const results = document.querySelector('.results')
+results.textContent = "Make a selection, first to five wins!";
+
 let computerWins = 0;
 let playerWins = 0;
 
-const results = document.querySelector('.results')
-
 function playRound(playerSelection, computerSelection) {
-    
-   computerSelection = computerPlay();
-    const final = playerSelection[0].toUpperCase() + playerSelection.substring(1)
-    results.textContent = `${final}, ${computerSelection}`
-    if(final === computerSelection) {
-        return "Tie, try again"
-    } else if(final === "Rock" && computerSelection !== "Paper") {
-        playerWins++;
-        return `Player wins, player: ${playerWins} computer: ${computerWins}`
-    } else if(final === "Paper" && computerSelection !== "Scissors"){
-        playerWins++;
-        return `Player wins, player: ${playerWins} computer: ${computerWins}`
-    } else if(final === "Scissors" && computerSelection !== "Rock"){
-        playerWins++;
-        return `Player wins, player: ${playerWins} computer: ${computerWins}`
-    } else {
-        computerWins++;
-        return `Computer wins, player: ${playerWins} computer: ${computerWins}`
-    }
-  }
 
-// Add event listener to buttons
+
+    const playerScore = document.querySelector('.playerScore');
+    const computerScore = document.querySelector('.computerScore');
+
+    computerSelection = computerPlay();
+
+    if(playerSelection === computerSelection) {
+        results.textContent = `Player chose the same as the computer, pick again.`
+    } else if(playerSelection === "Rock" && computerSelection !== "Paper") {
+        results.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}, player wins.`
+        playerWins+= 1;
+    } else if(playerSelection === "Paper" && computerSelection !== "Scissors"){
+        results.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}, player wins.`
+        playerWins++;
+    } else if(playerSelection === "Scissors" && computerSelection !== "Rock"){
+        results.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}, player wins.`
+        playerWins++;
+    } else {
+        results.textContent = `Player chose ${playerSelection}, Computer chose ${computerSelection}, computer wins.`
+        computerWins++;
+    }
+
+    
+
+    if(computerWins === 5) {
+    results.textContent = `GAME OVER, COMPUTER WINS`
+    document.querySelector('.rock').disabled = true;
+    document.querySelector('.paper').disabled = true;
+    document.querySelector('.scissors').disabled = true;
+    document.querySelector('.play-again').style.visibility = 'visible';
+    computerWins = 0;
+    playerWins = 0;
+    } if(playerWins === 5) {
+    results.textContent = `CONGRATULATIONS, YOU BEAT THE COMPUTER`
+    document.querySelector('.rock').disabled = true;
+    document.querySelector('.paper').disabled = true;
+    document.querySelector('.scissors').disabled = true;
+    document.querySelector('.play-again').style.visibility = 'visible';
+    computerWins = 0;
+    playerWins = 0;
+    }
+    playerScore.textContent = `Player: ${playerWins}`
+    computerScore.textContent = `Computer: ${computerWins}`
+}
+
+const playAgain = document.querySelector('.play-again');
+
+playAgain.addEventListener('click', function (){
+    document.querySelector('.rock').disabled = false;
+    document.querySelector('.paper').disabled = false;
+    document.querySelector('.scissors').disabled = false;
+    playAgain.style.visibility = 'hidden'
+    computerSelection = '';
+    playerSelection = ''
+})
+
+// Add event listener to buttons, get user input
+
 const container = document.querySelector('.container')
 container.addEventListener('click', function(e) {
-    const isButton = e.target.nodeName === 'BUTTON';
-    if (!isButton) {
-        return;
-      } else {
-
-    console.log(e.target.textContent)
-      }
+const isButton = e.target.nodeName === 'BUTTON';
+if (!isButton || e.target.className === "play-again") {
+    return;
+  } else {
+      playerSelection = e.target.textContent;
+      playRound(playerSelection);
+  }
 })
